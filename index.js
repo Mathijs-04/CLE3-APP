bef9576690c55dfd9ca8802801172897f2ad966a
J <junocraane@gmail.com>
2024-03-26 12:18:56 +0100

    ADDED INDEX FUNCTIONS (REFERENCE TO OTHER PAGE)

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const opgeslagenRoutes = document.getElementById('button1');
+opgeslagenRoutes.addEventListener('click', controlFunction);
+
+const nieuweRoutes = document.getElementById('button2');
+nieuweRoutes.addEventListener('click', controlFunction);
+
+function controlFunction() {
+    console.log('clicked on button');
+}
