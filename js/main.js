f3859ecf93d78ca2b6ef299208d2da7a7c487342
J <junocraane@gmail.com>
2024-03-27 11:03:37 +0100

    start of second day

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ window.addEventListener('load', init);
 function init() {
     const backButton = document.getElementById('back-button');
     backButton.addEventListener('click', sendBack);
+    const submitButton = document.getElementById('submit-button');
+    submitButton.addEventListener('click', dataGetter);
+
 }
 
 
@@ -11,3 +14,15 @@ function sendBack (){
     console.log('clicked on back button');
     window.history.back();
 }
+
+function dataGetter () {
+    const info = document.getElementsByTagName('input');
+    console.log(info);
+}
+
+
+//Input value in forms,
+    //Store value in variable,
+        //Variable in  array.
+
+//Haal
