8109c7c878ca11b13fb38b54c7bc89fcbc451023
J <junocraane@gmail.com>
2024-03-28 11:49:53 +0100

    ADDED LOCALSTORAGE ADDER.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', init);
 
+let reisdata = JSON.parse(localStorage.getItem('pairs')) || [];
+
 function init() {
     const backButton = document.getElementById('back-button');
     backButton.addEventListener('click', sendBack);
@@ -15,9 +17,25 @@ function sendBack (){
     window.history.back();
 }
 
-function dataGetter () {
-    const info = document.getElementsByTagName('input');
-    console.log(info);
+function dataGetter (event) {
+    event.preventDefault();
+    const infoVan = document.getElementById('van');
+    const infoVanValue = infoVan.value;
+    console.log(infoVanValue);
+    const infoNaar = document.getElementById('naar');
+    const infoNaarValue = infoNaar.value;
+    console.log(infoNaarValue);
+
+    dataStorer(infoVanValue, infoNaarValue);
+}
+
+
+function dataStorer (infoVanValue, infoNaarValue) {
+    const dataAdd = {van: infoVanValue, naar: infoNaarValue};
+
+    reisdata.push(dataAdd);
+
+    localStorage.setItem('reisdata', JSON.stringify(reisdata));
 }
 
 
