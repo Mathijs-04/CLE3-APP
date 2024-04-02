9bc4dc607866ba2865fdf984335a48fddd357781
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-04-02 11:26:52 +0200

    JavaScript Updated

--- js/main.js
+++ js/main.js
@@ -1,45 +1,46 @@
 document.addEventListener('DOMContentLoaded', init);
 
-let reisdata = JSON.parse(localStorage.getItem('reisdata')) || [];
+let reisdata = JSON.parse(localStorage.getItem('pairs')) || [];
 
 function init() {
     const backButton = document.getElementById('back-button');
-    const submitButton = document.getElementById('submit');
-    const opslaanCheckbox = document.getElementById('opslaan');
+    const submitButton = document.getElementById('submit-button');
 
-    if (backButton) {
+    if(backButton) {
         backButton.addEventListener('click', sendBack);
+    } else {
+        console.error('Back button not found');
     }
 
-    if (submitButton) {
-        submitButton.addEventListener('click', function (event) {
-            if (opslaanCheckbox && opslaanCheckbox.checked) {
-                event.preventDefault();
-                dataGetter();
-                localStorage.setItem('opslaan', 'true');
-            } else {
-                localStorage.setItem('opslaan', 'false');
-            }
-        });
+    if(submitButton) {
+        submitButton.addEventListener('click', dataGetter);
+    } else {
+        console.error('Submit button not found');
     }
 }
 
-function sendBack() {
+function sendBack (){
+    console.log('clicked on back button');
     window.history.back();
 }
 
-function dataGetter() {
+function dataGetter (event) {
+    event.preventDefault(event);
     const infoVan = document.getElementById('van');
-    const infoNaar = document.getElementById('naar');
     const infoVanValue = infoVan.value;
+    console.log(infoVanValue);
+    const infoNaar = document.getElementById('naar');
     const infoNaarValue = infoNaar.value;
+    console.log(infoNaarValue);
 
     dataStorer(infoVanValue, infoNaarValue);
+    window.location.href = 'favorieteroutes.html';
 }
 
-function dataStorer(infoVanValue, infoNaarValue) {
+function dataStorer (infoVanValue, infoNaarValue) {
     const dataAdd = {van: infoVanValue, naar: infoNaarValue};
 
     reisdata.push(dataAdd);
+
     localStorage.setItem('reisdata', JSON.stringify(reisdata));
 }
