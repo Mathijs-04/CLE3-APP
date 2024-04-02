d4eaf484020761c3d9a407fe1b6f7b296fd7b9c5
J <junocraane@gmail.com>
2024-04-02 11:05:06 +0200

    FAVORIETEROUTES.HTTML GEFIXXXXX

--- js/favorieteroutes.js
+++ js/favorieteroutes.js
@@ -8,7 +8,8 @@ let main;
 function init() {
     main = document.querySelector('main');
     if (main) {
-        getFromLocalStorage();
+        getFromLocalStorage()
+
     } else {
         console.error('Main element not found');
     }
@@ -18,18 +19,36 @@ function getFromLocalStorage() {
     const data = JSON.parse(localStorage.getItem('reisdata'));
     favoriteRoutesData = data || [];
     createDivs();
+    const deleteButtons = document.querySelectorAll('.delete');
+    deleteButtons.forEach(button => button.addEventListener('click', removeFromLocalStorage));
+
 }
 
 function createDivs() {
-    for (let i = 0; i < favoriteRoutesData.length; i++) {
+    main.innerHTML = '';
+    favoriteRoutesData.forEach(routeData => {
         const div = document.createElement('div');
+        div.classList.add('route');
         main.appendChild(div);
-        fillDivs(div, favoriteRoutesData[i]);
-    }
+        fillDivs(div, routeData);
+    });
 }
 
 function fillDivs(div, data) {
-    const p = document.createElement('p');
-    div.appendChild(p);
-    p.innerHTML = `${data.van}, ${data.naar}`;
+    const deleteButton = document.createElement('p');
+    deleteButton.classList.add('delete');
+    deleteButton.innerText = 'X';
+    div.appendChild(deleteButton);
+
+    const fromTo = document.createElement('p');
+    fromTo.innerText = `${data.van} > ${data.naar}`;
+    div.appendChild(fromTo);
+}
+
+function removeFromLocalStorage(event) {
+    console.log('testCLICKed');
+   const index = Array.from(this.parentNode.parentNode.children).indexOf(this.parentNode);
+    favoriteRoutesData.splice(index, 1);
+    localStorage.setItem('reisdata', JSON.stringify(favoriteRoutesData));
+    createDivs();
 }
