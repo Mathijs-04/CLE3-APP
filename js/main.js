cf1074a8851c0e4534618b7cf5976212453a930b
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-04-02 10:43:34 +0200

    main.js edited

--- js/main.js
+++ js/main.js
@@ -5,39 +5,37 @@ let reisdata = JSON.parse(localStorage.getItem('pairs')) || [];
 function init() {
     const backButton = document.getElementById('back-button');
     const submitButton = document.getElementById('submit-button');
+    const opslaanCheckbox = document.getElementById('opslaan');
 
-    if(backButton) {
+    if (backButton) {
         backButton.addEventListener('click', sendBack);
-    } else {
-        console.error('Back button not found');
     }
 
-    if(submitButton) {
-        submitButton.addEventListener('click', dataGetter);
-    } else {
-        console.error('Submit button not found');
+    if (submitButton) {
+        submitButton.addEventListener('click', function () {
+            if (opslaanCheckbox && opslaanCheckbox.checked) {
+                dataGetter();
+            }
+        });
     }
 }
 
-function sendBack (){
-    console.log('clicked on back button');
+function sendBack() {
     window.history.back();
 }
 
-function dataGetter (event) {
+function dataGetter(event) {
     event.preventDefault(event);
     const infoVan = document.getElementById('van');
     const infoVanValue = infoVan.value;
-    console.log(infoVanValue);
     const infoNaar = document.getElementById('naar');
     const infoNaarValue = infoNaar.value;
-    console.log(infoNaarValue);
 
     dataStorer(infoVanValue, infoNaarValue);
     window.location.href = 'favorieteroutes.html';
 }
 
-function dataStorer (infoVanValue, infoNaarValue) {
+function dataStorer(infoVanValue, infoNaarValue) {
     const dataAdd = {van: infoVanValue, naar: infoNaarValue};
 
     reisdata.push(dataAdd);
