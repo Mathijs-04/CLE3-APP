e9d3f000c2466d254e6c1c4882752208f712ee4f
ItsMySquid <dirkvangelderen0529@gmail.com>
2024-04-03 14:14:34 +0200

    Engelse namen

--- js/instellingen.js
+++ js/instellingen.js
@@ -1,15 +1,8 @@
 window.addEventListener("load",init)
 
-// Globals
-let chosenColor;
-
-
 function init(){
     let colorForm = document.getElementById("form");
     colorForm.addEventListener("input", colorSelector)
-
-    setChosenColorFromLocalStorage();
-
 }
 function colorSelector(e) {
     chosenColor = e.target.value;
@@ -17,20 +10,4 @@ function colorSelector(e) {
     // Sla de geselecteerde kleur op in de local storage
     localStorage.setItem("gekozenKleur", chosenColor);
     kleurMaker()
-}
-
-function setChosenColorFromLocalStorage() {
-    chosenColor = localStorage.getItem("gekozenKleur");
-
-    if (chosenColor) {
-        // Voeg de juiste class toe aan de elementen die een kleur nodig hebben
-        kleurMaker()
-    }
-}
-
-
-function kleurMaker() {
-    let selectHeader = document.querySelector("header")
-    selectHeader.className = '';
-    selectHeader.classList.add(`header-${chosenColor}`)
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', init);
 
 let reisdata = JSON.parse(localStorage.getItem('pairs')) || [];
+let chosenColor;
 
 function init() {
     getFromLocalStorage();
@@ -18,6 +19,8 @@ function init() {
     } else {
         console.error('Submit button not found');
     }
+
+    setChosenColorFromLocalStorage();
 }
 
 function sendBack (){
@@ -65,4 +68,29 @@ function dataStorer(infoVanValue, infoNaarValue) {
 function getFromLocalStorage() {
     const data = JSON.parse(localStorage.getItem('reisdata'));
     favoriteRoutesData = data || [];
+}
+
+function setChosenColorFromLocalStorage() {
+    chosenColor = localStorage.getItem("gekozenKleur");
+
+    if (chosenColor) {
+        // Voeg de juiste class toe aan de elementen die een kleur nodig hebben
+        kleurMaker()
+    }
+}
+
+function kleurMaker() {
+    let selectHeader = document.querySelector("header")
+    selectHeader.className = '';
+    selectHeader.classList.add(`header-${chosenColor}`)
+
+    let backbutton = document.querySelector('img[src="./img/Arrow-Blue.png"]');
+
+    if (backbutton && chosenColor) {
+        // Vervang "Blue" door de opgeslagen kleur
+        let newImage = backbutton.src.replace("Blue", chosenColor);
+
+        // Pas de bron van de afbeelding aan met de nieuwe kleur
+        backbutton.src = newImage;
+    }
 }
