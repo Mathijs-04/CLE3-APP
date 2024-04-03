f11c222f3609f37f2652e48a2b0c92e1e280adeb
ItsMySquid <dirkvangelderen0529@gmail.com>
2024-04-03 13:44:49 +0200

    Engelse namen

--- js/instellingen.js
+++ js/instellingen.js
@@ -7,14 +7,11 @@ let chosenColor;
 function init(){
     let colorForm = document.getElementById("form");
     colorForm.addEventListener("input", colorSelector)
-    console.log(colorForm)
 
     setChosenColorFromLocalStorage();
 
 }
 function colorSelector(e) {
-    console.log(e.target.value)
-
     chosenColor = e.target.value;
 
     // Sla de geselecteerde kleur op in de local storage
@@ -27,7 +24,6 @@ function setChosenColorFromLocalStorage() {
 
     if (chosenColor) {
         // Voeg de juiste class toe aan de elementen die een kleur nodig hebben
-        console.log(chosenColor);
         kleurMaker()
     }
 }
@@ -37,14 +33,4 @@ function kleurMaker() {
     let selectHeader = document.querySelector("header")
     selectHeader.className = '';
     selectHeader.classList.add(`header-${chosenColor}`)
-
-    if (chosenColor === "paars") {
-        console.log("De kleur is paars")
-    } else if (chosenColor === "groen") {
-        console.log("De kleur is Groen")
-    } else if (chosenColor === "roze") {
-        console.log("De kleur is Roze")
-    } else if (chosenColor === "blauw") {
-        console.log("De kleur is Blauw")
-    }
 }
