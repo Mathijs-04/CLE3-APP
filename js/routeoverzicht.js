73b63a4bd16ebf59e413630f67805608626cc3b2
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-04-05 10:52:37 +0200

    routeoverzicht.js edited

--- js/routeoverzicht.js
+++ js/routeoverzicht.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', init);
 
-let date = document.getElementById('date');
-let time = document.getElementById('time');
-let route = document.getElementById('route');
-
 function init() {
-    console.log(date);
-    console.log(time);
-    console.log(route);
+    let date = document.getElementById('date');
+    let time = document.getElementById('time');
+    let route = document.getElementById('route');
+
+    if (date && time && route) {
+        console.log(date.textContent);
+        console.log(time.textContent);
+        console.log(route.textContent);
+    } else {
+        console.log('One or more elements not found');
+    }
 }
